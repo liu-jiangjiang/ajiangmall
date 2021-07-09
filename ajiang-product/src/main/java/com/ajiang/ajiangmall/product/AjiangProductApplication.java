84a8a036279ba76bfd1c7f1ec111f3d0a2725af9
刘江写代码养你呀 <173024756@qq.com>
2021-07-09 23:03:02 +0800

    test

---
+++ ajiang-product/src/main/java/com/ajiang/ajiangmall/product/AjiangProductApplication.java
@@ -0,0 +1,13 @@
+package com.ajiang.ajiangmall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjiangProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjiangProductApplication.class, args);
+	}
+
+}
